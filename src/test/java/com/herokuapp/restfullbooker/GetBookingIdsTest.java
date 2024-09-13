3eca3bd9eb9dc77eb04c1872a7c2a24d6e521fcc
Yuliia Porkhun <julposv@gmail.com>
2024-09-13 14:53:12 +0300

    GetBookingIdsTest is created

---
+++ src/test/java/com/herokuapp/restfullbooker/GetBookingIdsTest.java
@@ -0,0 +1,25 @@
+package com.herokuapp.restfullbooker;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.List;
+
+
+public class GetBookingIdsTest {
+
+    //Java coding style
+    @Test
+    public void getBookingIdsTest() {
+        //Get response from booking ids
+        Response response = RestAssured.get("https://restful-booker.herokuapp.com/booking");
+        response.print();
+
+        //Make sure that response with code 200
+        Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200 but is not");
+
+        //Make sure that response isn't empty and has a booking id
+        List<Integer> bookingIDs = response.jsonPath().getList("bookingid");
+        Assert.assertFalse(bookingIDs.isEmpty(), "List of bookings is empty but shouldn't be");
+    }
+}
