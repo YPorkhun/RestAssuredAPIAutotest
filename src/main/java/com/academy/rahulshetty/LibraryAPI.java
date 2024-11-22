4c5c86c7324bcfb3931f80d66952cdbc4e6a9414
Yuliia Porkhun <julposv@gmail.com>
2024-11-22 18:36:44 +0200

    DataProvider added for LibraryAPI class

--- src/main/java/com/academy/rahulshetty/LibraryAPI.java
+++ src/main/java/com/academy/rahulshetty/LibraryAPI.java
@@ -4,19 +4,26 @@ import com.academy.rahulshetty.files.Requests;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LibraryAPI {
-    @Test
-    public void addBook () {
+    @Test (dataProvider = "BooksData")
+    public void addBook (String isbn , String aisle) {
         RestAssured.baseURI = "http://216.10.245.166";
         String responseAddBook = given().header("Content-Type","application/json")
-                .body(Requests.AddBook())
+                .body(Requests.AddBook(isbn,aisle))
                 .when()
                 .post("/Library/Addbook.php")
                 .then().assertThat().statusCode(200).extract().response().asString();
         JsonPath js = new JsonPath(responseAddBook);
         String bookID = js.getString("ID");
         System.out.println("Unique ID: " + bookID);
+    }
+
+    @DataProvider (name = "BooksData")
+    public Object[][] getData ()
+    {
+       return new Object[][] {{"bla","123"},{"kla","456"},{"mla","789"}};
     }
 }
